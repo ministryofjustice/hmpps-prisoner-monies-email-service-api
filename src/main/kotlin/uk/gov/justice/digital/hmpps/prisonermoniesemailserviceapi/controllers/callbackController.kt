004b3645c3ccf66d927b07f7364c9d63392e1700
Harriet-Hall <harriet.hall@justice.gov.uk>
2026-06-25 15:03:32 +0100

    TEST GREEN: Callback endpoint returns 204 No Content

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonermoniesemailserviceapi/controllers/callbackController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonermoniesemailserviceapi/controllers/callbackController.kt
@@ -1,14 +1,15 @@
 package uk.gov.justice.digital.hmpps.prisonermoniesemailserviceapi.controllers
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CallbackController {
 
   @PostMapping("/notify-callbacks")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   fun handleEmailCallback() {
-    val harriet = "cool"
-    print("Breakpoints are $harriet")
-    return
+
   }
 }
