acb7ca2c9864dfe72a4add8dbcf717701785b714
Harriet-Hall <harriet.hall@justice.gov.uk>
2026-06-25 12:23:00 +0100

    preauthorise handleEmailCallback method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonermoniesemailserviceapi/controllers/callbackController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonermoniesemailserviceapi/controllers/callbackController.kt
@@ -1,14 +1,15 @@
 package uk.gov.justice.digital.hmpps.prisonermoniesemailserviceapi.controllers
-
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CallbackController {
 
+  @PreAuthorize("hasRole('ROLE_PRISONER_MONIES_EMAIL_SERVICE_API__READ')")
   @PostMapping("/notify-callbacks")
   fun handleEmailCallback() {
+    val harriet = "cool"
     return
   }
-
 }
