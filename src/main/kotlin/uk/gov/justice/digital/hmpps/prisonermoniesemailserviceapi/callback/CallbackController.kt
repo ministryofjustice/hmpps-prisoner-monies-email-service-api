7ada4016eec9352b7c631001eea12565d260b1b3
AlHerbMoj <Alex.Herbert@justice.gov.uk>
2026-07-08 16:27:57 +0100

    Adding some guff to test copilot reviews

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonermoniesemailserviceapi/callback/CallbackController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonermoniesemailserviceapi/callback/CallbackController.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.prisonermoniesemailserviceapi.callback
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -16,4 +18,11 @@ class CallbackController(private val transmitter: NotifyAppInsightsLogger) {
   fun handleCallback(@RequestBody callback: NotifyCallbackRequest) {
     transmitter.emit(callback)
   }
+}
+
+@GetMapping("/alexTest")
+fun alexTest() {
+  val alex = "cool"
+  val i_hope = "this is caught in review"
+  ResponseEntity.ok("ok")
 }
